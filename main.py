78a7151b2737141df7bfaa10df8ab3083aa13bfd
christianfaccio <christianfaccio@outlook.it>
2026-03-10 16:17:47 +0100

    added book

--- main.py
+++ main.py
@@ -1,34 +1,45 @@
 from google import genai
-import os
+import time
+import random
+import os 
 from dotenv import load_dotenv
 
+load_dotenv()
 
-# Accessing API KEY in environment variables
-load_dotenv() 
-api_key = os.getenv("API_KEY")
-if api_key:
-    API_KEY = api_key
-print(api_key)
-
+API_KEY = os.getenv("GEMINI_API_KEY")
 MODEL_ID = "gemma-3-27b-it"
+NUM_ROUNDS = 1
 
 client = genai.Client(api_key=API_KEY)
-prompt = f"Write a very short but funny story of 4 or 5 sentences set in the XII century in France."
 
-try:
-    response = client.models.generate_content(
-        model=MODEL_ID,
-        contents=prompt,
-        config=genai.types.GenerateContentConfig(
-            temperature=1.0,
-            top_p=0.95
-        ),
-    )
-    
-    print(f"story: {response.text.strip()}")
-    print(f"tokens: {response.usage_metadata.total_token_count}")
+centuries = ["XII", "XVII", "XIX", "XXI", "XXV"]
+countries = ["Italy", "Vietnam", "Argentina", "Morocco", "Canada"]
 
-    print("-" * 50 + "\n")
+for i in range(1, NUM_ROUNDS + 1):
+    century = random.choice(centuries)
+    country = random.choice(countries)
     
-except Exception as e:
-    print(f"error in request: {e}")
+    prompt = f"Write a very short but funny story of 4 or 5 sentences set in the {century} century in {country}."
+    
+    try:
+        response = client.models.generate_content(
+            model=MODEL_ID,
+            contents=prompt,
+            config=genai.types.GenerateContentConfig(
+                temperature=1.0,
+                top_p=0.95
+            ),
+        )
+        
+        print(f"--- story number {i} ---")
+        print(f"context: {century} century in {country}")
+        print(f"story: {response.text.strip()}")
+        print(f"tokens: {response.usage_metadata.total_token_count}")
+
+        print("-" * 50 + "\n")
+        
+    except Exception as e:
+        print(f"error in request {i}: {e}")
+    
+    if i < NUM_ROUNDS:
+        time.sleep(5)
